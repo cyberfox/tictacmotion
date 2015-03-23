d7c79794a9665c4f7fcabe1c6b5e4ab33a659475
Morgan Schweers <cyberfox@gmail.com>
2015-03-22 17:27:52 -0700

    First commit of basic Tic Tac Toe game.

---
+++ app/tic_tac_toe_controller.rb
@@ -0,0 +1,66 @@
+class TicTacToeController < UIViewController
+  def viewDidLoad
+    each_width = (view.frame.size.width-20)/3
+
+    @board_view = UIView.alloc.initWithFrame([[0, 0], [each_width * 3 + 15, each_width * 3 + 15]])
+    @board_view.center = view.center
+    @board_view.backgroundColor = UIColor.whiteColor
+
+    @moves = []
+    [0, 1, 2].each do |row|
+      [0, 1, 2].each do |column|
+        square = UIView.alloc.initWithFrame([[0, 0], [each_width, each_width]])
+        square.center = [5 + row * (each_width + 2.5) + (each_width / 2), 5 + column * (each_width + 2.5) + (each_width / 2)]
+        square.backgroundColor = UIColor.blackColor
+        square.userInteractionEnabled = true
+        @moves << square
+        @board_view.addSubview square
+      end
+    end
+#    @board_view.userInteractionEnabled = false
+    view.addSubview @board_view
+#    view.userInteractionEnabled = false
+
+    @x_image = UIImage.imageNamed 'x.png'
+    @o_image = UIImage.imageNamed 'o.png'
+    @is_x = true
+    super
+  end
+
+  def viewWillAppear(animated)
+    becomeFirstResponder
+  end
+
+  def viewDidDisappear(animated)
+    super
+    resignFirstResponder
+  end
+
+  def canBecomeFirstResponder
+    true
+  end
+
+  def touchesBegan(touches, withEvent:event)
+    NSLog "Got event: %@", event
+    locationPoint = touches.anyObject.locationInView(self.view)
+    NSLog "Got location: %@", locationPoint
+    touched_view = self.view.hitTest(locationPoint, withEvent:event)
+#    touched_view.backgroundColor = UIColor.blueColor
+    touched_view.layer.contents = @is_x ? @x_image.CGImage : @o_image.CGImage
+    @is_x = !@is_x
+  end
+
+  def motionEnded(motion, withEvent:event)
+    if motion == UIEventSubtypeMotionShake
+      @moves.each { |square| square.backgroundColor = UIColor.blackColor; square.layer.contents = nil }
+    end
+  end
+
+  def shouldAutorotateToInterfaceOrientation
+    false
+  end
+
+  def shouldAutorotate
+    false
+  end
+end

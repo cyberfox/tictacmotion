509cc7ce3c0b3eece54343702f909e805670295b
Morgan Schweers <cyberfox@gmail.com>
2015-03-22 22:17:36 -0700

    Add some win conditions and a board state that needs to be refactored out at some point.

--- Rakefile
+++ Rakefile
@@ -12,4 +12,5 @@ Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'tictac'
   app.device_family = [:iphone, :ipad]
+  app.provisioning_profile = "/Users/mrs/Downloads/iOS_Team_Provisioning_Profile_.mobileprovision"
 end
--- app/tic_tac_toe_controller.rb
+++ app/tic_tac_toe_controller.rb
@@ -1,7 +1,19 @@
 class TicTacToeController < UIViewController
+  WIN_STATES = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+  ]
+
   def viewDidLoad
     each_width = (view.frame.size.width-20)/3
 
+    @board = " "*9
     @board_view = UIView.alloc.initWithFrame([[0, 0], [each_width * 3 + 15, each_width * 3 + 15]])
     @board_view.center = view.center
     @board_view.backgroundColor = UIColor.whiteColor
@@ -40,19 +52,34 @@ class TicTacToeController < UIViewController
     true
   end
 
+  def notify
+    UIAlertController.alert(self, 'This is happening, OK?', buttons: ['Cancel', 'Kill it!', 'Uh, what?']
+      ) do |button|
+      # button is 'Cancel', 'Kill it!' or 'Uh, what?'
+    end
+  end
+
   def touchesBegan(touches, withEvent:event)
     NSLog "Got event: %@", event
     locationPoint = touches.anyObject.locationInView(self.view)
     NSLog "Got location: %@", locationPoint
     touched_view = self.view.hitTest(locationPoint, withEvent:event)
+    touched = @moves.index(touched_view)
+    if touched
 #    touched_view.backgroundColor = UIColor.blueColor
-    touched_view.layer.contents = @is_x ? @x_image.CGImage : @o_image.CGImage
-    @is_x = !@is_x
+      touched_view.layer.contents = @is_x ? @x_image.CGImage : @o_image.CGImage
+      @board[touched] = @is_x ? 'x' : 'o'
+      # Check to see if anyone won the game.
+      # notify if @board[0..2] == 'xxx'
+      @is_x = !@is_x
+    end
   end
 
   def motionEnded(motion, withEvent:event)
     if motion == UIEventSubtypeMotionShake
+      @board = " "*9
       @moves.each { |square| square.backgroundColor = UIColor.blackColor; square.layer.contents = nil }
+      @is_x = true
     end
   end
 

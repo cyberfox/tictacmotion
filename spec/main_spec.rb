e39ab989f82d059a92a0009be1615ef329ae6f7c
Morgan Schweers <cyberfox@gmail.com>
2017-05-31 23:44:47 -0700

    Add a few small tests.

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,7 +3,15 @@ describe "Application 'tictac'" do
     @app = UIApplication.sharedApplication
   end
 
-  it "has one window" do
-    @app.windows.size.should == 1
+  it 'has two windows window' do
+    @app.windows.size.should == 2
+  end
+
+  it 'Does not show a nav bar' do
+    @app.windows.first.rootViewController.navigationBar.isHidden.should == true
+  end
+
+  it 'should have a TicTacToeController' do
+    @app.windows.first.rootViewController.topViewController.class.should == TicTacToeController
   end
 end
